0f10859f23da8bf826b59f7127b2b92b8e71a170
Lazy-Donut <lazy.donut@yandex.ru>
2024-11-24 23:16:13 +0600

    Media, media type search is added. Add contact functional is also added

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ require('dotenv').config();
 const {Bot, GrammyError, HttpError, Keyboard} = require('grammy');
 const sequelize = require('./db')
 const bot = new Bot(process.env.BOT_API_KEY);
-const {processToken, create, nameSearch, authorizationCheck} = require('./src/services')
+const {
+    processToken,
+    create,
+    nameSearch,
+    authorizationCheck,
+    mediaSearch,
+    mediaTypeSearch,
+    newContactMessage
+} = require('./src/services')
 const {startMessage, helpMessage} = require('./src/msgs')
 
 const chats = {};
+
+const contactModes = {};
+
+const newContacts = {};
 
 const start = async () => {
     try {
@@ -23,6 +35,7 @@ const start = async () => {
 
     bot.command('start', async (ctx) => {
         if (await authorizationCheck(ctx)) {
+            chats[ctx.chatId] = undefined;
             const keyboard = new Keyboard()
                 .text('Поиск по имени журналиста')
                 .text('Поиск по названию СМИ')
@@ -33,7 +46,8 @@ const start = async () => {
                 .text('Отправить сообщение модератору')
                 .resized()
 
-            await ctx.reply((`Привет, ${ctx.from.first_name}! Можно приступать к работе.`, {reply_markup: keyboard}))
+            await ctx.reply(`Привет, ${ctx.from.first_name}!`, {reply_markup: {remove_keyboard: true}})
+            await ctx.reply(`Можно приступать к работе.`, {reply_markup: keyboard})
         }
     })//приветственное сообщение при первом контакте с ботом после авторизации
 
@@ -43,16 +57,39 @@ const start = async () => {
         }
     }) //справочное сообщение при вводе /help
 
-    bot.hears('Поиск по имени', async (ctx) => {
+    bot.hears('Поиск по имени журналиста', async (ctx) => {
         if (await authorizationCheck(ctx) === true) {
             chats[ctx.chatId] = 'name_search';
             await ctx.reply('Введите имя журналиста для поиска')
         }
     })
 
+    bot.hears('Поиск по названию СМИ', async (ctx) => {
+        if (await authorizationCheck(ctx) === true) {
+            chats[ctx.chatId] = 'media_search';
+            await ctx.reply('Введите название СМИ для поиска')
+        }
+    })
+
+    bot.hears('Поиск по виду СМИ', async (ctx) => {
+        if (await authorizationCheck(ctx) === true) {
+            chats[ctx.chatId] = 'media_type_search';
+            await ctx.reply('Введите вид СМИ для поиска')
+        }
+    })
+
+    bot.hears('Добавить новый контакт журналиста', async (ctx) => {
+        if (await authorizationCheck(ctx) === true) {
+            await ctx.reply(`Создание нового контакта`, {reply_markup: {remove_keyboard: true}})
+            chats[ctx.chatId] = 'new_contact';
+            contactModes[ctx.chatId] = await newContactMessage(ctx)
+            newContacts[ctx.chatId] = {};
+        }
+    })
+
+
     bot.command('create', async (ctx) => {
         chats[ctx.chatId] = 'creating'
-        console.log(chats)
         await create(ctx)
     })
 
@@ -60,6 +97,23 @@ const start = async () => {
             switch (chats[ctx.chatId]) {
                 case 'name_search':
                     nameSearch(ctx);
+                    break;
+                case 'media_search':
+                    mediaSearch(ctx);
+                    break;
+                case'media_type_search':
+                    mediaTypeSearch(ctx);
+                    break;
+                case 'new_contact':
+                    const mode = contactModes[ctx.chatId]
+                    newContacts[ctx.chatId][mode] = ctx.msg.text;
+                    contactModes[ctx.chatId] = await newContactMessage(ctx, mode)
+                    if (mode === 'telegram_username') {
+                        await create(ctx, newContacts[ctx.chatId])
+                        chats[ctx.chatId] = undefined;
+                        contactModes[ctx.chatId] = undefined;
+                        newContacts[ctx.chatId] = undefined;
+                    }
                     break;
                 case undefined:
                     await authorizationCheck(ctx, false) === true ? await ctx.reply('Выберите пункт меню для дальнейшей работы') : processToken(ctx);
--- src/services.js
+++ src/services.js
@@ -3,11 +3,12 @@ const {TokenModel, UserModel, ContactModel} = require('../model')
 //const {Bot, GrammyError, HttpError, Keyboard} = require('grammy');
 const sequelize = require('../db')
 const {firstMessage, startMessage} = require('./msgs')
+const {Keyboard} = require("grammy");
 
 const authorizationCheck = async (ctx, withMessage = true) => {
     const user = await UserModel.findOne({where: {telegram_id: ctx.from.id}});
     if (user === null && withMessage) {
-        await ctx.reply((startMessage), { parse_mode: "HTML" })
+        await ctx.reply((startMessage), {parse_mode: "HTML"})
     }
     return user !== null;
 }
@@ -29,38 +30,95 @@ const processToken = async (ctx) => {
     }
 }
 
-const create = async (ctx) => {
+const create = async (ctx, contactData) => {
+    Object.keys(contactData).forEach(key => {
+        if (contactData[key] === 'Пропустить ввод') {
+            contactData[key] = null
+        }
+    })
     if (await authorizationCheck(ctx) === true) {
-        const contact = await ContactModel.create({
-            media: 'Test_media123',
-            first_name: 'Vasya',
-            last_name: 'Pupkin',
-            media_type: 'Moscow',
-            telegram_username: '@test_user',
-            email: 'test@example.com',
-            phone_number: '+7 (999) 999-99-99',
-            social_media_link: 'https://test_social_media.com',
-        })
+        const contact = await ContactModel.create(contactData)
         const user = await UserModel.findOne({where: {telegram_id: ctx.from.id}});
         contact.setUser(user)
         contact.save()
-        await ctx.reply('Новая запись создана')
+        await ctx.reply('Новая запись создана', {reply_markup: {remove_keyboard: true}})
     }
 }
 
-const nameSearch = async (ctx) => {
-    const searchString = ctx.msg.text
-    const query = 'SELECT * FROM contacts WHERE CONCAT(first_name, " ", last_name) like "%' + searchString + '%"';
+const searchContacts = async (ctx, query) => {
     const contacts = await sequelize.query(query, {
         model: ContactModel,
         mapToModel: true, // pass true here if you have any mapped fields
     });
-
+    if (contacts.length === 0) {
+        await ctx.reply('Ничего не найдено')
+        return;
+    }
     contacts.forEach((contact) =>
         ctx.reply((
-            `<b><i>Имя</i></b>&#128231;: ${contact.first_name}\nФамилия: ${contact.last_name}\nСМИ: ${contact.media}\nТип СМИ: ${contact.media_type}\nTelegram: ${contact.telegram_username}\nПочта: ${contact.email}\nТелефон: ${contact.phone_number}\nСоцсети: ${contact.social_media_link}`),
-    { parse_mode: "HTML" }))
+                `<b><i>Имя</i></b>: ${contact.first_name}\nФамилия: ${contact.last_name}\nСМИ: ${contact.media}\nТип СМИ: ${contact.media_type}\nTelegram: ${contact.telegram_username}\nПочта: ${contact.email}\nТелефон: ${contact.phone_number}\nСоцсети: ${contact.social_media_link}`),
+            {parse_mode: "HTML"}))
 }
 
+const nameSearch = async (ctx) => {
+    await searchContacts(ctx, 'SELECT * FROM contacts WHERE CONCAT(first_name, " ", COALESCE(last_name, "")) like "%' + ctx.msg.text + '%"')
+}
 
-module.exports = {processToken, create, authorizationCheck, nameSearch};
+const mediaSearch = async (ctx) => {
+    await searchContacts(ctx, 'SELECT * FROM contacts WHERE media like "%' + ctx.msg.text + '%"')
+}
+
+const mediaTypeSearch = async (ctx) => {
+    await searchContacts(ctx, 'SELECT * FROM contacts WHERE media_type like "%' + ctx.msg.text + '%"')
+}
+
+const newContactMessage = async (ctx, mode = null) => {
+    let message = null
+    let nextMode = null
+    switch (mode) {
+        case null:
+            message = 'Введите имя журналиста'
+            nextMode = 'first_name';
+            break;
+        case 'first_name':
+            message = 'Введите фамилию журналиста'
+            nextMode = 'last_name';
+            break;
+        case 'last_name':
+            message = 'Введите название СМИ'
+            nextMode = 'media';
+            break;
+        case 'media':
+            message = 'Введите вид СМИ (например, деловые, лайфстайл)'
+            nextMode = 'media_type';
+            break;
+        case 'media_type':
+            message = 'Введите ссылку профиль журналиста в соцсетях:
+            nextMode = 'social_media_link';
+            break;
+        case 'social_media_link':
+            message = 'Введите email журналиста'
+            nextMode = 'email';
+            break;
+        case 'email':
+            message = 'Введите номер телефона журналиста'
+            nextMode = 'phone_number';
+            break;
+        case 'phone_number':
+            message = 'Введите контакт журналиста в телеграме'
+            nextMode = 'telegram_username'
+            break
+        case 'telegram_username':
+            message = 'Данные введены успешно'
+            nextMode = 'done'
+            break;
+    }
+    const keyboard = new Keyboard()
+        .text('Пропустить ввод')
+        .resized()
+
+    await ctx.reply(message, nextMode === 'first_name' ? {} : {reply_markup: keyboard})
+    return nextMode;
+}
+
+module.exports = {processToken, create, authorizationCheck, nameSearch, mediaSearch, mediaTypeSearch, newContactMessage};
